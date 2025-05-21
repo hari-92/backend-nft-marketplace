49a21f7f7c3eeee7ac85b3e2b8f22ea8b3547983
hari-92 <dev.hainguyen92@gmail.com>
2025-05-21 09:32:30 +0700

    api with role

--- src/internal/modules/gateway/routers/auth.go
+++ src/internal/modules/gateway/routers/auth.go
@@ -2,7 +2,11 @@ package gateway_routers
 
 func (p RegisterRoutersIn) RegisterAuthRoutes() {
 	group := p.Engine.Group("/auth")
+
+	// Guest
 	group.POST("/login", p.AuthController.Login)
+
+	// User
 	group.POST("/register", p.AuthController.Register)
 	group.POST("/logout", p.AuthController.Logout)
 
