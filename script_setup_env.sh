b674a33cfbb300c2797a67e1d8f210e082130d6d
hari-92 <121082301+hari-92@users.noreply.github.com>
2025-05-21 09:33:56 +0700

    check if installed (#20)

--- script_setup_env.sh
+++ script_setup_env.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
 
-# Install protoc compiler
-PB_REL="https://github.com/protocolbuffers/protobuf/releases"
-curl -LO $PB_REL/download/v30.2/protoc-30.2-linux-x86_64.zip
-unzip protoc-30.2-linux-x86_64.zip -d $HOME/.local
+# Check if protoc is already installed
+if ! command -v protoc &> /dev/null; then
+    echo "Installing protoc compiler..."
+    
+    # Install protoc compiler
+    PB_REL="https://github.com/protocolbuffers/protobuf/releases"
+    curl -LO $PB_REL/download/v30.2/protoc-30.2-linux-x86_64.zip
+    unzip protoc-30.2-linux-x86_64.zip -d $HOME/.local
 
-# Add protoc to PATH
-echo 'export PATH="$PATH:$HOME/.local/bin"' >> ~/.bashrc
-source ~/.bashrc
+    # Add protoc to PATH
+    echo 'export PATH="$PATH:$HOME/.local/bin"' >> ~/.bashrc
+    source ~/.bashrc
+    
+    echo "Protoc installation completed!"
+else
+    echo "Protoc is already installed. Skipping installation."
+fi
